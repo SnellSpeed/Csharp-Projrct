d83befd04ea429ca2c11eb82af1984fd6b97ac16
Rohan RoyChowdhury <imrohan1997@gmail.com>
2022-03-30 13:07:11 +0530

    csharp

---
+++ CsharpProject/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace CsharpProject
+{
+    class test
+    {
+        public static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int c = a + b;
+            Console.WriteLine("{0} + {1} = {2}",a,b,c);
+
+        }
+
+    }
+
+}
